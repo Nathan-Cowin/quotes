359f92c6ee5085c8eb9e41de39c5f34d20b66db8
Nathan Cowin <natha.cowin@icede.co.uk>
2024-08-23 00:47:45 +0100

    add auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -20,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'api_token'
     ];
 
     /**
@@ -43,5 +45,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function generateToken()
+    {
+        $this->api_token = Str::random(60);
+        //todo encrypt the token
+        $this->save();
+
+        return $this->api_token;
     }
 }
