01a9aa3958d8ce788c3da531baebc6bb6b60ca77
Nathan Cowin <natha.cowin@icede.co.uk>
2024-08-23 00:46:03 +0100

    update routes

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,11 @@
 <?php
 
 use App\Http\Controllers\QuoteController;
-use Illuminate\Http\Request;
+use App\Http\Middleware\AuthenticateWithApiToken;
 use Illuminate\Support\Facades\Route;
-use App\Facades\Quote;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-//todo refactor to group
-Route::get('/{name}/quotes/{count?}', [QuoteController::class, 'getQuotesWithoutRefresh']);
-Route::get('/{name}/quotes/{count?}/refresh', [QuoteController::class, 'getRefreshedQuotes']);
-
-Route::get('/quotes/refresh', function (Request $request) {
-    Quote::driver('kayne')
-    ->clearSaved()
-    ->setCount(5)
-    ->getQuotes();
-
-//    return response()->json([
-//        'data' => 'ok'
-//    ]);
+Route::middleware([AuthenticateWithApiToken::class])->group(function () {
+    Route::get('/{name}/quotes/{count?}', [QuoteController::class, 'getQuotesWithoutRefresh']);
+    Route::get('/{name}/quotes/{count?}/refresh', [QuoteController::class, 'getRefreshedQuotes']);
 });
